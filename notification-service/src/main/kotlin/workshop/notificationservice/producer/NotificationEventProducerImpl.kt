f1370e1f8b2d456ed31fb71fa3e374b9cc02e218
Anders Kvamsøe <anders.kvamsoe@telenor.no>
2025-09-17 11:26:17 +0200

    Add missing annotations

--- notification-service/src/main/kotlin/workshop/notificationservice/producer/NotificationEventProducerImpl.kt
+++ notification-service/src/main/kotlin/workshop/notificationservice/producer/NotificationEventProducerImpl.kt
@@ -1,8 +1,10 @@
 package workshop.notificationservice.producer
 
+import org.springframework.stereotype.Component
 import workshop.notificationservice.dto.NotificationEvent
 
 // TODO: TASK 6
+@Component
 class NotificationEventProducerImpl : NotificationEventProducer {
     override fun produce(notificationEvent: NotificationEvent) {
         TODO("TASK 6: Not yet implemented")
