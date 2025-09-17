0438e72ee8c7f9ea1d6968507e89d5a1b1f5981d
Anders Kvamsøe <anders.kvamsoe@telenor.no>
2025-09-17 09:57:14 +0200

    Add required imports as comments

--- ticket-service/src/main/kotlin/workshop/ticketservice/producer/TicketEventProducer.kt
+++ ticket-service/src/main/kotlin/workshop/ticketservice/producer/TicketEventProducer.kt
@@ -2,8 +2,6 @@ package workshop.ticketservice.producer
 
 import workshop.ticketservice.dto.TicketEvent
 
-// TODO: Task 3
-
 interface TicketEventProducer {
     fun produce(ticketEvent: TicketEvent)
 }
